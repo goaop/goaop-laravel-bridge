7aad911b3fb9c215ffefc00b6bb9fa1d1463ac97
Kirill Nesmeyanov <nesk@xakep.ru>
2019-05-30 17:42:03 +0300

    Improve cofiguration file
    1) Typo.
    2) More description info was added.
    3) Added excluding of "App/Exceptions/*"

--- config/go_aop.php
+++ config/go_aop.php
@@ -1,91 +1,132 @@
 <?php
-
 /*
- |--------------------------------------------------------------------------
- | Go! AOP Configuration
- |--------------------------------------------------------------------------
- |
+ * Go! AOP framework configuration for Laravel.
+ *
+ * This source file is subject to the license that is bundled
+ * with this source code in the file LICENSE.
  */
-use Go\Core\GoAspectContainer;
+declare(strict_types=1);
+
 
 return [
+
     /*
      |--------------------------------------------------------------------------
      | AOP Debug Mode
      |--------------------------------------------------------------------------
      |
-     | When AOP is in debug mode, then breakpoints in the original source code
-     | will work. Also engine will refresh cache files if the original files were
-     | changed.
-     | For production mode, no extra filemtime checks and better integration with opcache
+     | When AOP is in debug mode, then breakpoints in the original source
+     | code will work. Also engine will refresh cache files if the original
+     | files were changed.
+     |
+     | For production mode, no extra filemtime checks and better
+     | integration with opcache.
      |
      */
-    'debug' => env('APP_DEBUG', false),
+
+    'debug' => env('GOAOP_DEBUG', env('APP_DEBUG', false)),
 
     /*
-     |--------------------------------------------------------------------------
-     | Application root directory
-     |--------------------------------------------------------------------------
-     |
-     | AOP will be applied only to the files in this directory, change it to app_path()
-     | if needed
-     */
+    |--------------------------------------------------------------------------
+    | Application Root Directory
+    |--------------------------------------------------------------------------
+    |
+    | AOP will be applied only to the files in this directory, change it
+    | to app_path() if needed.
+    |
+    */
+
     'appDir' => base_path(),
 
     /*
-     |--------------------------------------------------------------------------
-     | AOP cache directory
-     |--------------------------------------------------------------------------
-     |
-     | AOP engine will put all transformed files and caches in that directory
-     */
-    'cacheDir' => storage_path('app/aspect'),
+    |--------------------------------------------------------------------------
+    | AOP Cache Directory
+    |--------------------------------------------------------------------------
+    |
+    | AOP engine will put all transformed files and caches in that directory.
+    |
+    */
+
+    'cacheDir' => env('GOAOP_CACHE_DIR', storage_path('app/aspect')),
 
     /*
-     |--------------------------------------------------------------------------
-     | Cache file mode
-     |--------------------------------------------------------------------------
-     |
-     | If configured then will be used as cache file mode for chmod
-     */
-    'cacheFileMode' => 511,
+    |--------------------------------------------------------------------------
+    | Cache File Mode
+    |--------------------------------------------------------------------------
+    |
+    | If configured then will be used as cache file mode for chmod.
+    |
+    */
+
+    'cacheFileMode' => env('GOAOP_CACHE_PERMISSIONS', 511),
 
     /*
-     |--------------------------------------------------------------------------
-     | Controls miscellaneous features of AOP engine
-     |--------------------------------------------------------------------------
-     |
-     | See \Go\Aop\Features enumeration for bit mask
-     */
-    'features' => 0,
+    |--------------------------------------------------------------------------
+    | Miscellaneous AOP Engine Features
+    |--------------------------------------------------------------------------
+    |
+    | This option should contain a bitmask of values defined in
+    | \Go\Aop\Features enumeration:
+    |
+    | Support Options:
+    |   1  - enables interception of system function.
+    |   2  - enables interception of "new" operator in the source code.
+    |   4  - enables interception of "include"/"require" operations
+    |       in legacy code.
+    |   64 - do not check the cache presence and assume that cache
+    |       is already prepared
+    |
+    | <code>
+    |   //
+    |   // bitmask of 1 + 2 + 4 options.
+    |   //
+    |   'features' => 1 | 2 | 4,
+    |
+    | </code>
+    |
+    */
+
+    'features' => env('GOAOP_FEATURES', 0),
 
     /*
-     |--------------------------------------------------------------------------
-     | White list of directories
-     |--------------------------------------------------------------------------
-     |
-     | AOP will check this list to apply an AOP to selected directories only,
-     | leave it empty if you want AOP to be applied to all files in the appDir
-     */
+    |--------------------------------------------------------------------------
+    | Directories White List
+    |--------------------------------------------------------------------------
+    |
+    | AOP will check this list to apply an AOP to selected directories only,
+    | leave it empty if you want AOP to be applied to all files in the appDir.
+    |
+    */
+
     'includePaths' => [
-        app('path')
+        app_path()
     ],
 
     /*
-     |--------------------------------------------------------------------------
-     | Black list of directories
-     |--------------------------------------------------------------------------
-     |
-     | AOP will check this list to disable AOP for selected directories
-     */
-    'excludePaths' => [],
+    |--------------------------------------------------------------------------
+    | Directories Black List
+    |--------------------------------------------------------------------------
+    |
+    | AOP will check this list to disable AOP for selected directories.
+    |
+    | Note: The App\Exceptions\Handler SHOULD NOT be handled by the
+    | GO! AOP framework, since in case of fatal errors, it will not be
+    | possible to correctly process an Exception.
+    |
+    */
+
+    'excludePaths' => [
+        app_path('Exceptions')
+    ],
 
     /*
-     |--------------------------------------------------------------------------
-     | AOP container class
-     |--------------------------------------------------------------------------
-     |
-     | This option can be useful for extension and fine-tuning of services
-     */
-    'containerClass' => GoAspectContainer::class,
+    |--------------------------------------------------------------------------
+    | AOP Container
+    |--------------------------------------------------------------------------
+    |
+    | This option can be useful for extension and fine-tuning of services.
+    |
+    */
+
+    'containerClass' => \Go\Core\GoAspectContainer::class,
 ];
